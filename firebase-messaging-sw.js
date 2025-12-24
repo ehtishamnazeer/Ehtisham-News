3d548fc0ef7d549220bdad31a0d85d9aafb8d8f8
ehtishamnazeer <ehtishamnazeer54@gmail.com>
2025-12-24 12:40:37 +0500

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,1 +1,9 @@
-console.log("Service Worker Active");
+importScripts('https://www.gstatic.com/firebasejs/8.10.0/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/8.10.0/firebase-messaging.js');
+
+firebase.initializeApp({
+    messagingSenderId: "951936357732"
+});
+
+const messaging = firebase.messaging();
+console.log("Service Worker Active & Firebase Ready");
