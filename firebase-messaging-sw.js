e09eb045960348002c384cd6d8ef08fadf580e34
ehtishamnazeer <ehtishamnazeer54@gmail.com>
2025-12-24 12:12:25 +0500

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,13 +1,14 @@
 importScripts('https://www.gstatic.com/firebasejs/8.10.0/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/8.10.0/firebase-messaging.js');
 
-firebase.initializeApp({
+const firebaseConfig = {
     apiKey: "AIzaSyC9Cfh60ZyeZTbhK45a_sKWBFCy8DrqDao",
     projectId: "ehtisham-news",
     messagingSenderId: "951936357732",
     appId: "1:951936357732:web:75039f69741e974e384f32"
-});
+};
 
+firebase.initializeApp(firebaseConfig);
 const messaging = firebase.messaging();
 
 messaging.onBackgroundMessage((payload) => {
